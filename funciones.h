05af229a1f6e2681c720f11064082176588d95ad
matefer1603 <130242764+matefer1603@users.noreply.github.com>
2023-07-09 17:55:25 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,2 +1,9 @@
+#define maxP 100
+
+typedef struct {
+    char nombre[50];
+    float precio;
+} Producto;
+
 void agregarProducto(Producto *productos, int *numProductos);
 void mostrarFactura(Producto *productos, int numProductos);
